ce48b0d652839077b5d933b391c388ace8e5526d
binlduu25 <binlduu25@gmail.com>
2025-09-10 17:41:49 +0900

    Add files via upload

---
+++ 01_Java/00_javaBasic/src/edu/kh/basic/JavaTest3.java
@@ -0,0 +1,9 @@
+package edu.kh.basic;
+
+public class JavaTest3{
+	public static void main(String[] args) {
+		System.out.println("546564");
+		//syso 후 ctrl+space는 println 구문 자동완성 단축키
+		System.out.println("25423d");
+	}
+}
